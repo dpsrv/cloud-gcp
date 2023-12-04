093e26546e29901ede05cdd6269ebc03d2ef5b16
Max <max@maxf.net>
2023-12-04 16:23:03 -0500

    updated

--- bin/push-init.sh
+++ bin/push-init.sh
@@ -8,6 +8,27 @@ if [ -z $env ]; then
 	exit 1
 fi
 
-ssh dpsrv@dpsrv-$env mkdir .config
+ssh dpsrv@dpsrv-$env bash <<_EOT_
+	[ -d .config ] || mkdir .config
+_EOT_
+
 scp -r ~/.config/git/ dpsrv@dpsrv-$env:.config/
 
+ssh dpsrv@dpsrv-$env bash <<_EOT_
+
+	cd /mnt/disks/data/opt
+	[ -d git-openssl-secrets ] || git clone https://github.com/maxfortun/git-openssl-secrets.git
+
+	[ -d dpsrv ] || mkdir dpsrv
+	cd dpsrv
+	
+	for repo in rc nginx; do
+		[ ! -d $repo ] || continue
+		git clone https://github.com/dpsrv/$repo.git 
+		if grep -q openssl $repo/.gitattributes; then
+			/mnt/disks/data/opt/git-openssl-secret/git-init-openssl-secrets.sh
+			cd $repo
+		fi
+	done
+
+_EOT_
